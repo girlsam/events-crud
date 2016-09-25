c9bfabab24ff1f6b334e6a43e8cb2fcabc359eb0
Sam Goldsmith <samggold@gmail.com>
2016-09-24 23:06:53 -0600

    venue promise added to single-event get route

--- src/server/routes/events.js
+++ src/server/routes/events.js
@@ -17,14 +17,19 @@ router.get('/:id/register', (req, res, next) => {
   const id = parseInt(req.params.id);
   let getEvent = knex('events').where('events.id', id).first();
   let getTickets = knex.raw('SELECT tickets.event_id, tickets.id, tickets.price, tickets.name, events.title FROM tickets JOIN events ON events.id=tickets.event_id');
+  // let getTicketCount = ('SELECT tickets.event_id, tickets.id FROM tickets JOIN events ON events.id=tickets.event_id');
+  let getVenue = knex.raw('SELECT venues.capacity, venues.id, events.title FROM venues JOIN events ON venues.id=events.venue_id');
   Promise.all([
     getEvent,
-    getTickets
+    getTickets,
+    getVenue
   ])
   .then(results => {
     const renderObject = {};
     renderObject.events = results[0];
     renderObject.tickets = results[1].rows;
+    renderObject.venues = results[2].rows;
+    console.log(renderObject.venues);
     res.render('events/register', renderObject);
   }).catch(err => {
     console.log(err);
