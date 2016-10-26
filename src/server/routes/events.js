773444d39a77a9f4422311100b03a83ac56215ce
Sam Goldsmith <samggold@gmail.com>
2016-10-26 16:37:45 -0600

    POST route complete for registration

--- src/server/routes/events.js
+++ src/server/routes/events.js
@@ -16,8 +16,14 @@ router.get('/:id/register', (req, res, next) => {
   const eventID = parseInt(req.params.id);
   let getEvent = knex('events').where('events.id', eventID).first();
   let getTickets = knex.raw('SELECT tickets.event_id, tickets.id, tickets.price, tickets.name, events.title FROM tickets JOIN events ON events.id=tickets.event_id');
-  let getVenue = knex('venues').select('venues.capacity', 'events.id', 'events.venue_id').join('events', {'venues.id': 'events.venue_id'});
-  let getGuestCount = knex.select('tickets_attendees.attendee_id', 'events.id').from('tickets_attendees').join('tickets', {'tickets_attendees.ticket_id': 'tickets.id'}).join('events', {'tickets.event_id': 'events.id'}).where('events.id', eventID);
+  let getVenue = knex('venues')
+  .select('venues.capacity', 'events.id', 'events.venue_id')
+  .join('events', {'venues.id': 'events.venue_id'});
+  let getGuestCount = knex.select('tickets_attendees.attendee_id', 'events.id')
+  .from('tickets_attendees')
+  .join('tickets', {'tickets_attendees.ticket_id': 'tickets.id'})
+  .join('events', {'tickets.event_id': 'events.id'})
+  .where('events.id', eventID);
   Promise.all([
     getEvent,
     getTickets,
@@ -36,24 +42,23 @@ router.get('/:id/register', (req, res, next) => {
   });
 });
 
-router.post(':id/register/new', (req, res, next) => {
+//add hidden field with ticket id
+router.post('/:id/register', (req, res, next) => {
   let eventID = req.params.id;
-  console.log(eventID);
+  let ticketID = req.body.ticket_type;
   let newRegister = {
     preferred_name: req.body.preferred_name,
     last_name: req.body.last_name,
     birthday: req.body.birthday,
     email: req.body.email
   };
-  let newTicket = {
-    name: req.body.ticket_name
-  };
-  let registeredEvent = {
-    id: eventID
-  };
-  console.log('new register', newRegister);
-  console.log('new ticket', newTicket);
-  console.log('registered event', registeredEvent);
+  knex('attendees').insert(newRegister, 'id')
+  .then(([id]) => {
+    return knex('tickets_attendees').insert({'attendee_id': id, 'ticket_id': ticketID})
+  }).then(() => res.redirect('/events/' + eventID + '/register'))
+  .catch((err) => {
+    console.log(err);
+  });
 });
 
 module.exports = router;
